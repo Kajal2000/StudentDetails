d175cdf309d1b2600cef6c52427732ce23e913c6
kajal <you@example.com>
2020-04-30 01:03:16 +0530

    added superAdim

--- Rotues/admin.js
+++ Rotues/admin.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const admin = express.Router();
+const admin = express();
 const adminDB  = require("../Model/adminDB")
 
 
--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.use("/Apis",data1)
 const data2 = require("./Rotues/admin")
 app.use("/Api",data2)
 
+// const data3 = require("./Rotues/superAdmin")
+// app.use("/superApi",data3)
+
 app.listen(8000,()=>{
     console.log("server is listening..............)")
 });
